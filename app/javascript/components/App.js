96a2c09814532a34b53cc26c6373bbb140251a76
Ionut <bostanionut28@gmail.com>
2022-03-14 09:42:25 +0000

    added reactJS

---
+++ app/javascript/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    render () {
+        return(
+            <div>Hello from the App this is ReactJs</div>
+        )
+    }
+}
+
+export default App 
