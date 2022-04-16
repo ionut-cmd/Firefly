b5de1a64c690f98d5c8c7367fe879a845000f422
Taimoor-R <taimoorriz@hotmail.com>
2022-04-16 03:25:17 +0100

    Added columns to personalize user data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_003249) do
+ActiveRecord::Schema.define(version: 2022_04_16_022050) do
 
   create_table "forums", force: :cascade do |t|
     t.string "title", null: false
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2022_04_14_003249) do
     t.string "current_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "firstname", null: false
+    t.string "lastname", null: false
+    t.string "thumbnail"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
