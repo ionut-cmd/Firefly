efb2a890c56441b84a3704e9f3ff7dbca3aa06e8
Taimoor-R <taimoorriz@hotmail.com>
2022-04-18 06:27:59 +0100

    Made New Nav Bar and ensured responsiveness

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   root 'home#home', as: 'homepage_index'
+  get 'aboutus', to: 'home#aboutus'
   get 'forum' , to:  'forums#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
