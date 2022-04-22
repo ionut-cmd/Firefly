c2d8f78894909788df962945513291c78a2224c5
Taimoor-R <taimoorriz@hotmail.com>
2022-04-22 01:57:27 +0100

    Fixed issues

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,4 @@ Rails.application.routes.draw do
 #   get 'aboutus_new', to: 'home#aboutus_new'
 # =======
   get 'profile', to: 'devise#edit'
-<<<<<<< HEAD
-
-
-=======
-# >>>>>>> 26bf5b11cdc2a39423c5df7643670d3137dec216
->>>>>>> refs/remotes/origin/develop
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
